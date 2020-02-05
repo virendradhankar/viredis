ec45b4a9e729f2b1e1cb69352921dd771981acb1
FuncGuy <tim25659@gmail.com>
2020-02-05 08:34:28 +0530

    Addressed review comments-2

--- src/main/java/vi/viredis/pool/RedisPool.java
+++ src/main/java/vi/viredis/pool/RedisPool.java
@@ -69,7 +69,6 @@ public class RedisPool implements Pool {
      */
     @Override
     public ViRedis get() throws RedisException {
-
         if(queue.size() == 0)
             throw new RedisException("pool capacity is reached not able to get new connection");
              if (!isShutdown) {
